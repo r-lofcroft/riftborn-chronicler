886044495d1da5165f6a44f0d39b2c36e4715eaf
Robin Williscroft <robin.williscroft@gmail.com>
2021-07-05 16:27:38 +0200

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
+
+  const [state, setState] =
+    useState(function generateRandomInt() {
+      return Math.floor(Math.random() * 100);
+    });
+  function clickHandler(e) {
+    setState(Math.floor(Math.random() * 100));
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chronicler</h1>
+      <h2>The adventures of the Riftborn Six.</h2>
+      <Button variant="secondary" onClick={clickHandler}>{state}</Button>
     </div>
   );
 }
